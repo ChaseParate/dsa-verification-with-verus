3be5a5135cde74dc02de0998149aa49372d68624
Chase Harkcom <me@chasehark.com>
2026-03-03 22:19:57 -0700

    Make some progress on selection sort (swapping seems to work!)

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -16,4 +16,23 @@ pub open spec fn is_valid_sorting_algorithm(input: Seq<i32>, output: Seq<i32>) -
     is_sorted(output) && is_permutation(input, output)
 }
 
+pub exec fn swap(input: &mut Vec<i32>, i: usize, j: usize)
+    requires
+        i < old(input).len(),
+        j < old(input).len(),
+    ensures
+        old(input).len() == input.len(),
+        old(input)[i as int] == input[j as int],
+        old(input)[j as int] == input[i as int],
+        forall|k: int|
+            0 <= k < input.len() && k != i as int && k != j as int ==> old(input)[k] == input[k],
+        is_permutation(old(input)@, input@),
+{
+    broadcast use vstd::seq_lib::group_to_multiset_ensures;
+
+    let temp = input[i];
+    input[i] = input[j];
+    input[j] = temp;
+}
+
 } // verus!
--- src/sort/selection.rs
+++ src/sort/selection.rs
@@ -2,22 +2,46 @@ use vstd::prelude::*;
 
 verus! {
 
-use super::is_valid_sorting_algorithm;
+use super::{is_permutation, is_valid_sorting_algorithm, swap};
 
-pub fn selection_sort(input: Vec<i32>) -> (output: Vec<i32>)
+pub exec fn selection_sort(input: &mut Vec<i32>)
     ensures
-        is_valid_sorting_algorithm(input@, output@),
+        is_valid_sorting_algorithm(old(input)@, input@),
 {
-    // TODO
-    let output = input;
-    assume(is_valid_sorting_algorithm(input@, output@));
+    if input.is_empty() {
+        return ;
+    }
+    for i in 0..(input.len() - 1)
+        invariant
+            0 <= i < input.len(),
+    {
+        assert(0 <= i < input.len());
 
-    output
+        let mut max_index = i;
+        assert(0 <= max_index < input.len());
+
+        for j in (i + 1)..input.len()
+            invariant
+                i <= max_index < input.len(),
+        {
+            assert(0 <= j < input.len());
+            assert(0 <= max_index < input.len());
+
+            if input[j] > input[max_index] {
+                max_index = j;
+            }
+        }
+
+        swap(input, i, max_index);
+    }
 }
 
 fn main() {
-    let v = vec![2, 4, -5, 1, 3, 2];
-    let v_sorted = selection_sort(v);
+    let mut v = vec![2, 4, -5, 1, 3, 2];
+    let ghost old_v = v@;
+
+    selection_sort(&mut v);
+    let ghost new_v = v@;
 }
 
 } // verus!
