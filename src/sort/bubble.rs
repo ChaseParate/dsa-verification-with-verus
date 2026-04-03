45f1e4ae7362e4afa6b748cc1c48823634491b29
Chase Harkcom <me@chasehark.com>
2026-04-03 00:09:49 -0600

    Simplify loop invariants and stuff for bubble sort

--- src/sort/bubble.rs
+++ src/sort/bubble.rs
@@ -8,46 +8,37 @@ pub exec fn bubble_sort(input: &mut Vec<i32>)
     ensures
         is_valid_sorting_algorithm(old(input)@, input@),
 {
-    let len = input.len();
-    if len < 2 {
+    if input.is_empty() {
         return ;
     }
-    let mut i: usize = 0;
-    while i < len - 1
+    for i in iter: 0..(input.len() - 1)
         invariant
             input.len() == old(input).len(),
-            len == input.len(),
-            0 <= i <= len - 1,
+            iter.end == input.len() - 1,
+            0 <= i <= input.len() - 1,
             is_permutation(old(input)@, input@),
             forall|u: int, v: int|
-                (len as int - i as int) <= u < v < len as int ==> input[u] <= input[v],
+                (input.len() - i) <= u < v < input.len() ==> input[u] <= input[v],
             forall|k: int, m: int|
-                0 <= k < (len as int - i as int) && (len as int - i as int) <= m < len as int
-                    ==> input[k] <= input[m],
-        decreases len - i,
+                0 <= k < (input.len() - i) && (input.len() - i) <= m < input.len() ==> input[k]
+                    <= input[m],
     {
-        let mut j: usize = 0;
-        while j < len - i - 1
+        for j in iter: 0..(input.len() - i - 1)
             invariant
+                iter.end == input.len() - i - 1,
                 input.len() == old(input).len(),
-                len == input.len(),
-                0 <= i <= len - 1,
-                0 <= j <= len - i - 1,
                 is_permutation(old(input)@, input@),
                 forall|u: int, v: int|
-                    (len as int - i as int) <= u < v < len as int ==> input[u] <= input[v],
+                    (input.len() - i) <= u < v < input.len() ==> input[u] <= input[v],
                 forall|k: int, m: int|
-                    0 <= k < (len as int - i as int) && (len as int - i as int) <= m < len as int
-                        ==> input[k] <= input[m],
-                forall|k: int| 0 <= k <= j as int ==> input[k] <= input[j as int],
-            decreases (len as int - i as int - 1) - j as int,
+                    0 <= k < (input.len() - i) && (input.len() - i) <= m < input.len() ==> input[k]
+                        <= input[m],
+                forall|k: int| 0 <= k <= j ==> input[k] <= input[j as int],
         {
             if input[j] > input[j + 1] {
                 swap(input, j, j + 1);
             }
-            j += 1;
         }
-        i += 1;
     }
 }
 
