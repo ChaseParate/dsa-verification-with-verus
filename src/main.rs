8322ef25bc251a08cf8d7064c3879d9baa2ec53d
Chase Harkcom <me@chasehark.com>
2026-04-21 21:49:35 -0600

    Remove sample code from main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-use vstd::prelude::*;
-
 mod sort;
 
-verus! {
-
-spec fn min(x: int, y: int) -> int {
-    if x <= y {
-        x
-    } else {
-        y
-    }
-}
-
-fn main() {
-    assert(min(10, 20) == 10);
-    assert(min(-10, -20) == -20);
-    assert(forall|i: int, j: int| min(i, j) <= i && min(i, j) <= j);
-    assert(forall|i: int, j: int| min(i, j) == i || min(i, j) == j);
-    assert(forall|i: int, j: int| min(i, j) == min(j, i));
-    // assert(forall|i: int, j: int| min(i, j) == min(i, i));
-}
-
-} // verus!
+fn main() {}
